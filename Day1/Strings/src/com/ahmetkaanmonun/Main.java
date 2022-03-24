a63068cdf39b10b739df675a7e7eb709b0c96cdf
Kaan Monun <ahmetkaan.monun@outlook.com>
2022-03-24 12:50:24 +0300

    Update Main.java

--- Day1/Strings/src/com/ahmetkaanmonun/Main.java
+++ Day1/Strings/src/com/ahmetkaanmonun/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         // * String (It is actually a Class)
 
-        // ? Strings are immutable. That means you can't change a String after it's created. 05379552060
+        // ? Strings are immutable. That means you can't change a String after it's created.
 
         String myString = "This is a string";
 
